03a38fb3c21a9544c3ba6d6a2554011479ec3b57
sarahT04 <sarahtanujaya@gmail.com>
2024-02-12 13:27:09 +0700

    changed index to show current programs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,23 +3,38 @@ import Head from "next/head";
 import DetailPanel from "@/components/Frontpage/DetailPanel";
 import Banner from "@/components/Frontpage/Banner";
 import CoursesSection from "@/components/Frontpage/CoursesSection";
+import { getAllStorylinePrograms } from "@/lib/api";
+import { ProgramData } from "@/types/components";
 
-export default function Index() {
-    return (
-        <>
-            <Head><title>Pusakawan</title></Head>
-            <Headline />
-            <DetailPanel />
-            <CoursesSection />
-            <Banner />
-        </>
-    )
+type IndexProps = {
+  programs?: ProgramData[];
+};
+
+export default function Index({ programs }: IndexProps) {
+  return (
+    <>
+      <Head><title>Pusakawan</title></Head>
+      <Headline />
+      <DetailPanel />
+      <CoursesSection programs={programs} />
+      <Banner />
+    </>
+  )
 }
 
 export async function getStaticProps({ locale }: { locale: "en" | "id" }) {
+  const programsReq = await getAllStorylinePrograms();
+  if (programsReq && programsReq.status === 200) {
     return {
       props: {
         messages: (await import(`../locales/${locale}.json`)).default,
+        programs: programsReq.message.slice(0, 3)
       },
     };
   }
+  return {
+    props: {
+      messages: (await import(`../locales/${locale}.json`)).default,
+    },
+  };
+}
