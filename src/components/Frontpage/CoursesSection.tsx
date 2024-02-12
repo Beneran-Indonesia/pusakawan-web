deedc6c40b67790fd4c847faa068db4dad097a87
sarahT04 <sarahtanujaya@gmail.com>
2024-02-13 06:28:22 +0700

    mobile header done, courses section have bug

--- src/components/Frontpage/CoursesSection.tsx
+++ src/components/Frontpage/CoursesSection.tsx
@@ -43,12 +43,15 @@ export default function CoursesSection({ programs }: { programs?: ProgramData[];
 function CoursesCard({ data }: { data: undefined | ProgramData[] }) {
     const t = useTranslations('courses_selection');
     return (
+        <Box sx={{
+            overflowX: "auto",
+            whiteSpace: "nowrap"
+        }}>
         <Box
-            display="flex"
+            display="inline-flex"
             gap={12}
-            overflow="scroll"
         >
-            {
+           {
                 data?.length === 0 && coursesKey.map((cKey, idx) => (
                     <Card
                         key={cKey}
@@ -69,6 +72,7 @@ function CoursesCard({ data }: { data: undefined | ProgramData[] }) {
                     />
                 ))
             }
+        </Box>
         </Box>
     )
 }
--- src/components/MobileHeader.tsx
+++ src/components/MobileHeader.tsx
@@ -241,21 +241,11 @@ const HeaderMenu = styled((props: MenuProps) => (
     '& .MuiPaper-root': {
         borderRadius: 6,
         marginTop: theme.spacing(1),
-        // TODO:
-        minWidth: theme.breakpoints.up("md") ? 180 : "auto",
-        width: theme.breakpoints.up("md") ? "auto" : "fit-content",
+        width: "fit-content",
         boxShadow:
             'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
         '& .MuiMenu-list': {
             padding: '4px 0',
         },
-
-        '& .MuiMenuItem-root': {
-            '& .MuiSvgIcon-root': {
-                marginRight: theme.breakpoints.up("md") ? theme.spacing(1.5) : 0,
-            },
-
-        },
-
     },
 }));
