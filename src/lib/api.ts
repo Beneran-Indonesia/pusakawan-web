a21f54ed6ac3d3b7690993fb91dd7cd92ba02694
sarahT04 <sarahtanujaya@gmail.com>
2024-08-27 17:37:10 +0700

    https agent 3

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -8,10 +8,12 @@ const headers = {
     'Content-Type': 'application/json',
 }
 
+console.log(process.env.NEXT_PUBLIC_PRODUCTION)
+
 const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL,
     httpsAgent: new Agent({
-        rejectUnauthorized: process.env.NEXT_PUBLIC_PRODUCTION ? true : false
+        rejectUnauthorized: process.env.NEXT_PUBLIC_PRODUCTION === "true" ? true : false
     }),
     headers
 })
