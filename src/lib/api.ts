292aa8995a9c876320ff2bd9249de12d7bd106c5
sarahT04 <sarahtanujaya@gmail.com>
2024-09-09 18:18:14 +0800

    FIX: changes with index and readme

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -8,12 +8,10 @@ const headers = {
     'Content-Type': 'application/json',
 }
 
-console.log(process.env.NEXT_PUBLIC_PRODUCTION)
-
 const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL,
     httpsAgent: new Agent({
-        rejectUnauthorized: process.env.NEXT_PUBLIC_PRODUCTION === "true" ? true : false
+        rejectUnauthorized: process.env.NEXT_PUBLIC_PRODUCTION === "true"
     }),
     headers
 })
