52c765f242e567cbf472e6250b6234ba29911d11
sarahT04 <sarahtanujaya@gmail.com>
2025-02-22 12:31:40 +0800

    FEAT: BL1.6, privacy policy and tnc added on footer and sign in

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,79 +3,121 @@ import Box from "@mui/material/Box";
 import PusakawanLogo from "./PusakawanLogo";
 import { useTranslations } from "next-intl";
 import { footerInformation } from "@/lib/constants";
-import PhoneIcon from '@mui/icons-material/Phone';
-import PlaceIcon from '@mui/icons-material/Place';
+import PhoneIcon from "@mui/icons-material/Phone";
+import PlaceIcon from "@mui/icons-material/Place";
 import ChangeLanguageButtons from "./LanguageSwitcher";
-import EmailIcon from '@mui/icons-material/Email';
+import EmailIcon from "@mui/icons-material/Email";
 import { ReactNode } from "react";
 import { useDesktopRatio } from "@/lib/hooks";
+import { useState } from "react";
+import AgreementModal from "./AgreementsModal";
 
 export default function Footer() {
-    const t = useTranslations("footer");
-    const isDesktopRatio = useDesktopRatio();
-    return (
+  const [TCModalOpen, setTCModalOpen] = useState(false);
+  const t = useTranslations("footer");
+  const isDesktopRatio = useDesktopRatio();
+  return (
+    <Box
+      component="footer"
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        backgroundColor: "primary.main",
+        color: "white",
+        px: isDesktopRatio ? "10rem" : "1rem",
+      }}
+    >
+      <Box
+        display="flex"
+        my={isDesktopRatio ? 10 : 4}
+        justifyContent="space-between"
+        flexDirection={isDesktopRatio ? "row" : "column"}
+        gap={10}
+      >
+        <PusakawanLogo white={true} />
         <Box
-            component="footer"
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                backgroundColor: 'primary.main',
-                color: "white",
-                px: isDesktopRatio ? "10rem" : "1rem",
-            }}
+          display="flex"
+          color="white"
+          gap={isDesktopRatio ? 10 : 2}
+          flexDirection={isDesktopRatio ? "row" : "column"}
+          mt={isDesktopRatio ? 0 : 2}
         >
-            <Box
-                display="flex"
-                my={isDesktopRatio ? 10 : 4}
-                justifyContent="space-between"
-                flexDirection={isDesktopRatio ? "row" : "column"}
-                gap={10}
+          <Box aria-label="office address">
+            <Typography component="h5" variant="h5" fontWeight={500} my={1}>
+              {t("office_location")}
+            </Typography>
+            <FooterChild
+              label="address maps"
+              icon={<PlaceIcon />}
+              href={footerInformation.office.href}
+              value={footerInformation.office.value}
+            />
+          </Box>
+          <Box aria-label="contact">
+            <Typography component="h5" variant="h5" fontWeight={500} my={1}>
+              {t("contact_us")}
+            </Typography>
+            <FooterChild
+              label="phone number"
+              icon={<PhoneIcon />}
+              href={footerInformation.phone.href}
+              value={footerInformation.phone.value}
+            />
+            <FooterChild
+              label="email address"
+              icon={<EmailIcon />}
+              href={footerInformation.email.href}
+              value={footerInformation.email.value}
+            />
+            <Typography
+              sx={{ cursor: "pointer", my: 1 }}
+              fontWeight={500}
+              component="h4"
+              variant="h5"
+              title={t("tnc")}
+              aria-label="terms and conditions modal action"
+              onClick={() => setTCModalOpen(!TCModalOpen)}
             >
-                <PusakawanLogo white={true} />
-                <Box
-                    display="flex"
-                    color="white"
-                    gap={isDesktopRatio ? 10 : 2}
-                    flexDirection={isDesktopRatio ? "row" : "column"}
-                    mt={isDesktopRatio ? 0 : 2}
-                >
-                    <Box aria-label="office address">
-                        <Typography component="h5" variant="h5" fontWeight={500} my={1}>
-                            {t('office_location')}
-                        </Typography>
-                        <FooterChild label="address maps" icon={<PlaceIcon />} href={footerInformation.office.href} value={footerInformation.office.value} />
-                    </Box>
-                    <Box aria-label="contact">
-                        <Typography component="h5" variant="h5" fontWeight={500} my={1}>
-                            {t('contact_us')}
-                        </Typography>
-                        <FooterChild label="phone number" icon={<PhoneIcon />} href={footerInformation.phone.href} value={footerInformation.phone.value} />
-                        <FooterChild label="email address" icon={<EmailIcon />} href={footerInformation.email.href} value={footerInformation.email.value} />
-                        <ChangeLanguageButtons sx={{ m: 0, my: isDesktopRatio ? 0 : "0.5rem" }} />
-                        {/* <Typography>Syarat ketentuan</Typography> */}
-                    </Box>
-                </Box>
-            </Box>
-            <hr />
-            <Typography variant="h5" component="p" my={isDesktopRatio ? "75px" : "32px"} ml="auto">Copyrights &#169; 2024 Pusakawan </Typography>
+              {t("tnc")}
+            </Typography>
+            <AgreementModal
+              type="T&C"
+              open={TCModalOpen}
+              handleClose={() => setTCModalOpen(false)}
+            />
+            <ChangeLanguageButtons
+              sx={{ m: 0, my: isDesktopRatio ? 0 : "0.5rem" }}
+            />
+          </Box>
         </Box>
-    )
+      </Box>
+      <hr />
+      <Typography
+        variant="h5"
+        component="p"
+        my={isDesktopRatio ? "75px" : "32px"}
+        ml="auto"
+      >
+        Copyrights &#169; 2024 Pusakawan{" "}
+      </Typography>
+    </Box>
+  );
 }
 
 type FooterChildProps = {
-    icon: ReactNode;
-    href: string;
-    value: string;
-    label: string;
-}
+  icon: ReactNode;
+  href: string;
+  value: string;
+  label: string;
+};
 
 function FooterChild({ icon, href, value, label }: FooterChildProps) {
-    return (
-        <Box aria-label={label} display="flex" gap={1}>
-            <a rel="noreferrer" target="_blank" href={href}>{icon}</a>
-            <Typography>
-                {value}
-            </Typography>
-        </Box>
-    )
+  return (
+    <Box aria-label={label} display="flex" gap={1}>
+      <a rel="noreferrer" target="_blank" href={href}>
+        {icon}
+      </a>
+      <Typography>{value}</Typography>
+    </Box>
+  );
 }
