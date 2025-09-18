f20b1c3c7a206d0287560ce68b9132f311d909fd
sarahT04 <sarahtanujaya@gmail.com>
2025-09-18 23:33:17 +0800

    FIX: Enrollment still exists even if user did not pay

--- src/components/Card/Program.tsx
+++ src/components/Card/Program.tsx
@@ -7,30 +7,39 @@ import { useTranslations } from "next-intl";
 import { formatNumberToIdr } from "@/lib/utils";
 
 type CardProps = {
-    img: string;
-    title: string;
-    // programId: number;
-    // null price means that it's free ( not paid )
-    price: number;
-    href: string;
-}
+  img: string;
+  title: string;
+  // programId: number;
+  // null price means that it's free ( not paid )
+  price: number;
+  href: string;
+};
 
 export default function ProgramCard({ img, title, price, href }: CardProps) {
-    const t = useTranslations('program_selection');
-    return (
-        <CardWrapper alt={title} src={img}>
-            <Typography variant="h5" component="h5" fontWeight={500} mb={2} sx={{ whiteSpace: "break-spaces" }}>{title}</Typography>
-            <Typography variant="h3" component="h3" mb={4} fontWeight={600}>
-                {price === 0
-                    ? t('paid')
-                    : formatNumberToIdr(price)
-                }
-            </Typography>
-            <Button variant="outlined" size="small" href={href}
-                // onClick={onEnroll} disabled={!onEnroll}
-                sx={{ width: 'fit-content', mb: 3, alignSelf: 'center' }}>
-                    {t('button')}
-            </Button>
-        </CardWrapper>
-    )
+  const t = useTranslations("program_selection");
+  return (
+    <CardWrapper alt={title} src={img}>
+      <Typography
+        variant="h5"
+        component="h5"
+        fontWeight={500}
+        mb={2}
+        sx={{ whiteSpace: "break-spaces" }}
+      >
+        {title}
+      </Typography>
+      <Typography variant="h3" component="h3" mb={4} fontWeight={600}>
+        {price === 0 ? t("paid") : `Rp. ${formatNumberToIdr(price)}`}
+      </Typography>
+      <Button
+        variant="outlined"
+        size="small"
+        href={href}
+        // onClick={onEnroll} disabled={!onEnroll}
+        sx={{ width: "fit-content", mb: 3, alignSelf: "center" }}
+      >
+        {t("button")}
+      </Button>
+    </CardWrapper>
+  );
 }
